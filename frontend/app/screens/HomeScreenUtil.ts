1d9c6ac6e42159c5cc41ee777976f2692e0493cb
europanite <europanite@gmail.com>
2026-09-01 23:03:32 +0900

    first commit

--- frontend/app/screens/HomeScreenUtil.ts
+++ frontend/app/screens/HomeScreenUtil.ts
@@ -1,2 +1,2 @@
-export const REPO_URL = 'https://github.com/europanite/clinet-side-linear-programming-visualizer';
-export const REPO_URL_PAGE = 'https://europanite.github.io/clinet-side-linear-programming-visualizer/';
+export const REPO_URL = 'https://github.com/europanite/client-side-linear-programming-visualizer';
+export const REPO_URL_PAGE = 'https://europanite.github.io/client-side-linear-programming-visualizer/';
